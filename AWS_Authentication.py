c8055e02b169b7ab49ffa97d727bdd1e36bce90f
Tzu-hao <swing0313@yahoo.com.tw>
2019-11-05 14:47:28 +0800

    Add files via upload

---
+++ AWS_Authentication.py
@@ -0,0 +1,17 @@
+import boto3
+from botocore.client import Config
+
+ACCESS_KEY_ID = ''
+ACCESS_SECRET_KEY = ''
+BUCKET_NAME = ''
+
+data = open('test.png', 'rb')
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id = ACCESS_KEY_ID,
+    aws_secret_access_key = ACCESS_SECRET_KEY,
+    config = Config(signature_version = 's3v4')
+)
+
+s3.Bucket(BUCKET_NAME).put_object('test.png', Body=data)
+print('close')
