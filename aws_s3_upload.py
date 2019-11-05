38332ea12d973577e8d9a47bb0dff0b2538b2f30
Tzu-hao <swing0313@yahoo.com.tw>
2019-11-05 16:01:36 +0800

    Update and rename AWS_Authentication.py to aws_s3_upload.py

--- aws_s3_upload.py
+++ aws_s3_upload.py
@@ -10,8 +10,7 @@ s3 = boto3.resource(
     's3',
     aws_access_key_id = ACCESS_KEY_ID,
     aws_secret_access_key = ACCESS_SECRET_KEY,
-    config = Config(signature_version = 's3v4')
 )
 
-s3.Bucket(BUCKET_NAME).put_object('test.png', Body=data)
+s3.Bucket(BUCKET_NAME).put_object(Key='test.png', Body=data)
 print('close')
